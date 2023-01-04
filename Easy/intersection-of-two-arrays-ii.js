0922ab87744e4c530ad11b6b3cacdbe6b0f2d8eb
Adarsha_S <s.adarsharao@gmail.com>
2023-01-04 22:18:12 +0530

    Insertion of 2 arrays

---
+++ Easy/intersection-of-two-arrays-ii.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/submissions/871320634/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    const map = new Map();
+    for (const n of nums1) {
+        if (map.has(n)) {
+            map.set(n, map.get(n) + 1);
+        } else {
+            map.set(n, 1);
+        }
+    }
+    const result = [];
+    for (const n of nums2) {
+        if (map.has(n) && map.get(n) > 0) {
+            result.push(n);
+            map.set(n, map.get(n) - 1);
+        }
+    }
+    return result;
+};
